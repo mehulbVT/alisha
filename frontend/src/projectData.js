c9bf241e5bb107cfe223d7c44960576f08faa6d8
Alisha Bhale <alisha.bhale@gmail.com>
2023-04-25 12:42:13 +0530

    project data added

---
+++ frontend/src/projectData.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    title: 'Card View',
+    image: './images/card-view',
+    description: 'Card view for e-commerce.',
+    tools: ['Angular', 'TypeScript', 'SCSS', 'HTML', 'javaScript'],
+    github: 'https://github.com/alishabhale/cardView',
+    link: 'https://github.com/alishabhale/cardView#demo-video',
+  },
+  {
+    title: 'React Calculator',
+    image: './images/React-cal',
+    description: 'Basic Calculator using react',
+    tools: ['React', 'CSS', 'HTML'],
+    github: 'https://github.com/alishabhale/ReactCalculator',
+    link: 'https://festive-wilson-a51c32.netlify.app/',
+  }
+];
+
+export default projects;
